e50cd60ca9556b750f528cef2cdbbf8b850e79c3
srbulovicdusan <srbulovicdusan@gmail.com>
2020-02-18 19:26:31 +0100

    image classification

---
+++ ImageClasification/main.py
@@ -0,0 +1,174 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from sklearn import svm, metrics, datasets
+from sklearn.utils import Bunch
+from sklearn.model_selection import GridSearchCV, train_test_split
+import cv2
+from skimage.io import imread
+from skimage.transform import resize
+
+
+def load_image_files(container_path, dimension=(128, 128)):
+    """
+    Load image files with categories as subfolder names
+    which performs like scikit-learn sample dataset
+
+    Parameters
+    ----------
+    container_path : string or unicode
+        Path to the main folder holding one subfolder per category
+    dimension : tuple
+        size to which image are adjusted to
+
+    Returns
+    -------
+    Bunch
+    """
+    image_dir = Path(container_path)
+    folders = [directory for directory in image_dir.iterdir() if directory.is_dir()]
+    categories = [fo.name for fo in folders]
+
+    descr = "A image classification dataset"
+    images = []
+    flat_data = []
+    target = []
+
+
+
+    for i, direc in enumerate(folders):
+        for file in direc.iterdir():
+            print(i)
+
+            img = imread(str(file).replace('/', '//'))
+
+            img_resized = resize(img, dimension, anti_aliasing=True, mode='reflect')
+            flat_data.append(img_resized.flatten())
+            images.append(img_resized)
+            target.append(i)
+    flat_data = np.array(flat_data)
+    target = np.array(target)
+    images = np.array(images)
+
+    return Bunch(data=flat_data,
+                 target=target,
+                 target_names=categories,
+                 images=images,
+                 DESCR=descr)
+class Hog_descriptor():
+    def __init__(self, img, cell_size=16, bin_size=8):
+        self.img = img
+        self.img = np.sqrt(img / float(np.max(img)))
+        self.img = self.img * 255
+        self.cell_size = cell_size
+        self.bin_size = bin_size
+
+        self.angle_unit = 360 / self.bin_size
+        self.angle_unit = int(self.angle_unit)
+        assert type(self.bin_size) == int, "bin_size should be integer,"
+        assert type(self.cell_size) == int, "cell_size should be integer,"
+        assert type(self.angle_unit) == int, "bin_size should be divisible by 360"
+
+    def extract(self):
+        height, width = self.img.shape
+
+        gradient_magnitude, gradient_angle = self.global_gradient()
+        gradient_magnitude = abs(gradient_magnitude)
+        cell_gradient_vector = np.zeros((int(height / self.cell_size), int(width / self.cell_size), int(self.bin_size)))
+        for i in range(cell_gradient_vector.shape[0]):
+            for j in range(cell_gradient_vector.shape[1]):
+                cell_magnitude = gradient_magnitude[i * self.cell_size:(i + 1) * self.cell_size,
+                                 j * self.cell_size:(j + 1) * self.cell_size]
+                cell_angle = gradient_angle[i * self.cell_size:(i + 1) * self.cell_size,
+                             j * self.cell_size:(j + 1) * self.cell_size]
+                cell_gradient_vector[i][j] = self.cell_gradient(cell_magnitude, cell_angle)
+
+        hog_image = self.render_gradient(np.zeros([height, width]), cell_gradient_vector)
+        hog_vector = []
+        for i in range(cell_gradient_vector.shape[0] - 1):
+            for j in range(cell_gradient_vector.shape[1] - 1):
+                block_vector = []
+                block_vector.extend(cell_gradient_vector[i][j])
+                block_vector.extend(cell_gradient_vector[i][j + 1])
+                block_vector.extend(cell_gradient_vector[i + 1][j])
+                block_vector.extend(cell_gradient_vector[i + 1][j + 1])
+                mag = lambda vector: math.sqrt(sum(i ** 2 for i in vector))
+                magnitude = mag(block_vector)
+                if magnitude != 0:
+                    normalize = lambda block_vector, magnitude: [element / magnitude for element in block_vector]
+                    block_vector = normalize(block_vector, magnitude)
+                hog_vector.append(block_vector)
+        return hog_vector, hog_image
+
+    def global_gradient(self):
+        gradient_values_x = cv2.Sobel(self.img, cv2.CV_64F, 1, 0, ksize=5)
+        gradient_values_y = cv2.Sobel(self.img, cv2.CV_64F, 0, 1, ksize=5)
+        gradient_magnitude = cv2.addWeighted(gradient_values_x, 0.5, gradient_values_y, 0.5, 0)
+        gradient_angle = cv2.phase(gradient_values_x, gradient_values_y, angleInDegrees=True)
+        return gradient_magnitude, gradient_angle
+
+    def cell_gradient(self, cell_magnitude, cell_angle):
+        orientation_centers = [0] * self.bin_size
+        for i in range(cell_magnitude.shape[0]):
+            for j in range(cell_magnitude.shape[1]):
+                gradient_strength = cell_magnitude[i][j]
+                gradient_angle = cell_angle[i][j]
+                min_angle, max_angle, mod = self.get_closest_bins(gradient_angle)
+                orientation_centers[min_angle] += (gradient_strength * (1 - (mod / self.angle_unit)))
+                orientation_centers[max_angle] += (gradient_strength * (mod / self.angle_unit))
+        return orientation_centers
+
+    def get_closest_bins(self, gradient_angle):
+        idx = int(gradient_angle / self.angle_unit)
+        mod = gradient_angle % self.angle_unit
+        if idx == self.bin_size:
+            return idx - 1, (idx) % self.bin_size, mod
+        return idx, (idx + 1) % self.bin_size, mod
+
+    def render_gradient(self, image, cell_gradient):
+        cell_width = self.cell_size / 2
+        max_mag = np.array(cell_gradient).max()
+        for x in range(cell_gradient.shape[0]):
+            for y in range(cell_gradient.shape[1]):
+                cell_grad = cell_gradient[x][y]
+                cell_grad /= max_mag
+                angle = 0
+                angle_gap = self.angle_unit
+                for magnitude in cell_grad:
+                    angle_radian = math.radians(angle)
+                    x1 = int(x * self.cell_size + magnitude * cell_width * math.cos(angle_radian))
+                    y1 = int(y * self.cell_size + magnitude * cell_width * math.sin(angle_radian))
+                    x2 = int(x * self.cell_size - magnitude * cell_width * math.cos(angle_radian))
+                    y2 = int(y * self.cell_size - magnitude * cell_width * math.sin(angle_radian))
+                    cv2.line(image, (y1, x1), (y2, x2), int(255 * math.sqrt(magnitude)))
+                    angle += angle_gap
+        return image
+
+
+def main():
+    # image_dataset = load_image_files("images/")
+    # print(len(image_dataset.data));
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     image_dataset.data, image_dataset.target, test_size=0.3,random_state=109)
+    # param_grid = [
+    #   {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+    #   {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+    #  ]
+    # svc = svm.SVC()
+    # clf = GridSearchCV(svc, param_grid)
+    # clf.fit(X_train, y_train)
+    # y_pred = clf.predict(X_test)
+    # print("Classification report for - \n{}:\n{}\n".format(
+    #     clf, metrics.classification_report(y_test, y_pred)))
+    img = cv2.imread('images//dalmatian//image_0001.jpg', cv2.IMREAD_GRAYSCALE)
+    img = np.sqrt(img / float(np.max(img)))
+    img = img * 255
+    plt.imshow(img, cmap=plt.cm.gray)
+
+    print(img)
+    hog = Hog_descriptor(img, cell_size=8, bin_size=8)
+    vector, image = hog.extract()
+    #plt.imshow(image, cmap=plt.cm.gray)
+    plt.show()
+main()
---
+++ ImageClasification/proba.py
@@ -0,0 +1,118 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from sklearn import svm, metrics, datasets
+from sklearn.utils import Bunch
+from sklearn.model_selection import GridSearchCV, train_test_split
+import cv2
+from sklearn.preprocessing import Normalizer
+import csv
+def get_images(path ,csv_file):
+    with open(path + '//' + csv_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        images = []
+        labels = []
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                img = read_image(path + "//" + row[0])
+                images.append(img)
+                labels.append(row[1])
+                line_count += 1
+        return images, labels
+def read_image(path):
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    return img
+def resize_images(imgs, dimension=(128, 128)):
+
+    # resize image
+    resized_imgs = []
+    for img in imgs:
+        resized = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
+        resized_imgs.append(resized)
+    return resized_imgs
+def hog_compute(hog, imgs):
+    hog_features = []
+    a = 1
+    for img in imgs:
+        a += 1
+        hog_feature = hog.compute(img)
+        hog_features.append(hog_feature)
+    return hog_features
+def strToNumberLabels(label):
+    if (label == 'lilyvalley'):
+        return 1
+    elif (label == 'tigerlily'):
+        return 2
+    elif (label == 'snowdrop'):
+        return 3
+    elif (label == 'bluebell'):
+        return 4
+    elif (label == 'fritillary'):
+        return 4
+    else:
+        return -1
+
+
+def main():
+
+    hog = cv2.HOGDescriptor()
+
+    #GET IMAGES AND THEIR LABEL
+    print("Loading pictures...")
+    train_img, train_labels = get_images("train", "train_labels.csv");
+    test_img, test_labels = get_images("test", "test_labels.csv");
+    print("Loaded...")
+    #RESIZE
+    print("Resizing images...")
+    train_img = resize_images(train_img)
+    test_img = resize_images(test_img)
+    print("Resized...")
+    #MAP LABEL TO INT
+    train_labels = list(map(strToNumberLabels, train_labels))
+    test_labels = list(map(strToNumberLabels, test_labels))
+    #EXTRACT FEATURES
+    print("Before hog extraction")
+    features_train = hog_compute(hog, train_img)
+    features_test = hog_compute(hog, test_img)
+
+    print("passed hog extraction")
+    #
+    trainingDataMat = np.array(features_train)
+    labelsMat = np.array(train_labels)
+
+
+    svm = cv2.ml.SVM_create()
+    svm.setType(cv2.ml.SVM_C_SVC)
+    svm.setKernel(cv2.ml.SVM_LINEAR)
+
+
+    svm.setTermCriteria((cv2.TERM_CRITERIA_COUNT, 100, 1.e-10))
+
+    svm.train(trainingDataMat, cv2.ml.ROW_SAMPLE, labelsMat)
+    sample_data = np.array(features_test, np.float32)
+
+    svm.setC(100)
+    #svm.setGamma(0.1)
+    svm.train(trainingDataMat, cv2.ml.ROW_SAMPLE, labelsMat)
+    response = svm.predict(sample_data)
+    final = []
+    for y in response[1]:
+        final.append(int(y[0]))
+    countAccuracy(final, test_labels)
+
+
+
+def countAccuracy(result, test_labels):
+
+    counter = 0;
+    for i in range(len(result)):
+        if result[i] == test_labels[i]:
+            counter +=1
+    print("Acurracy: " + str(counter/len(result)))
+    return counter/len(result)
+
+main()
